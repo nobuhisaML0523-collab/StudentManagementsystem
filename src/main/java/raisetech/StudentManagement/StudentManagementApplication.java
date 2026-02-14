5b30b3b586a22382b7e476d547f6219b7b9f912a
Nobuhisa Kuramoto <nobuhisa.ML0523@outlook.jp>
2026-02-14 13:34:12 +0900

    20_JDBCとMyBatis(MAINクラスの不要インポート削除)

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,17 +1,8 @@
 package raisetech.StudentManagement;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class StudentManagementApplication {
