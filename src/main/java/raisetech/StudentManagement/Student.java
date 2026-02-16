1cd3cbe5793dbee464c77cedbf85d91f170092b8
Nobuhisa Kuramoto <nobuhisa.ML0523@outlook.jp>
2026-02-16 10:34:46 +0900

    23_MyBatisを使ってCRUDのRead処理を実装(新規作成)

--- src/main/java/raisetech/StudentManagement/Student.java
+++ src/main/java/raisetech/StudentManagement/Student.java
@@ -1,41 +1,17 @@
 package raisetech.StudentManagement;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Student {
+  private String id;
   private String name;
+  private String name_kana;
+  private String nickname;
+  private String email;
+  private String city;
   private int age;
-  private int id;
-
-  // MyBatis 無引数コンストラクタ
-  public Student() {
-  }
-
-  public Student(int id, String name, int age) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
+  private String gender;
 }
--- src/main/java/raisetech/StudentManagement/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/StudentRepository.java
@@ -1,31 +1,15 @@
 package raisetech.StudentManagement;
 
 import java.util.List;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface StudentRepository {
 
-  @Select("SELECT id, name, age FROM student")
-  List<Student> allStudents();
+  @Select("SELECT id, name, name_kana, nickname, email, city, age, gender FROM students")
+  List<Student> getStudentsList();
 
-  @Select("SELECT * FROM student WHERE id = #{id}")
-  Student searchById(@Param("id") int id);
-
-  @Insert("INSERT INTO student (id, name, age) values(#{id}, #{name}, #{age})")
-  void registerStudent(@Param("id") int id, @Param("name") String name, @Param("age") int age);
-
-  @Update("UPDATE student SET name = #{name} WHERE id = #{id}")
-  void updateName(@Param("id") int id,@Param("name") String name);
-
-  @Update("UPDATE student SET age = #{age} WHERE id = #{id}")
-  void updateAge(@Param("id") int id, @Param("age") int age);
-
-  @Delete("DELETE FROM student WHERE id = #{id}")
-  void deleteStudent(@Param("id") int id);
+  @Select("SELECT id, student_id, course_name, start_date, end_date FROM students_courses")
+  List<Course> getStudentsCourses();
 }
