6be70d6c867170feb69c1f4e96212a5d662f7cb8
Nobuhisa Kuramoto <nobuhisa.ML0523@outlook.jp>
2026-02-22 13:43:48 +0900

    29_受講生情報登録処理の実装(新規作成)

---
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -0,0 +1,50 @@
+package raisetech.StudentManagement.repository;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentsCourses;
+
+/**
+ * 受講生情報を扱うリポジトリ*
+ * 全件検索や単一条件での検索、コース情報の検索が行えるクラスです
+ */
+
+
+@Mapper
+public interface StudentRepository {
+
+  @Select("SELECT id, name, name_kana, nickname, email, city, age, gender, remark, is_deleted FROM students WHERE is_deleted = false")
+  List<Student> getStudentsList();
+
+  @Select("SELECT id, student_id, course_name, start_date, end_date FROM students_courses")
+  List<StudentsCourses> getStudentsCourses();
+
+  @Select("SELECT MAX(id) FROM students")
+  String findMaxStudentId();
+
+  @Select("SELECT MAX(id) FROM students_courses")
+  String findMaxStudentCourseId();
+
+  @Select("SELECT id, name, name_kana, nickname, email, city, age, gender, remark FROM students WHERE id = #{id} ")
+  Student findStudentById(String id);
+
+  @Select(" SELECT id, student_id, course_name, start_date, end_date FROM students_courses WHERE student_id = #{studentId} ")
+  List<StudentsCourses> findCoursesByStudentId(String studentId);
+
+  @Insert("INSERT INTO students(id, name, name_kana, nickname, email, city, age, gender, remark, is_deleted) VALUES(#{id}, #{name}, #{nameKana}, #{nickname}, #{email}, #{city}, #{age}, #{gender}, #{remark}, false)")
+  void insertStudent(Student student);
+
+  @Insert("INSERT INTO students_courses(id, student_id, course_name, start_date, end_date) VALUES(#{id}, #{studentId}, #{courseName}, #{startDate}, #{endDate})")
+  void insertStudentCourse(StudentsCourses course);
+
+  @Update("UPDATE students SET email = #{email}, city = #{city}, remark = #{remark} WHERE id = #{id}")
+  void updateStudent(Student student);
+
+  @Update(" UPDATE students SET is_deleted = true WHERE id = #{id} ")
+  void logicalDeleteStudent(String id);
+
+}
---
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -0,0 +1,99 @@
+package raisetech.StudentManagement.service;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.domain.StudentDetail;
+import raisetech.StudentManagement.repository.StudentRepository;
+
+@Service
+@Transactional
+public class StudentService {
+  private final StudentRepository repository;
+
+  @Autowired
+  public StudentService(StudentRepository repository){
+    this.repository = repository;
+  }
+
+  public List<Student> searchStudentsList() {
+    return repository.getStudentsList();
+  }
+
+  public List<StudentsCourses> searchStudentsCourses() {
+    return repository.getStudentsCourses();
+  }
+
+  public void registerStudent(Student student,StudentsCourses course) {
+
+    // =====================
+    // remark 未入力対策
+    // =====================
+    if (student.getRemark() == null || student.getRemark().isBlank()) {
+      student.setRemark("特になし");
+    }
+
+    // =====================
+    // student ID 採番
+    // =====================
+    String maxStudentId = repository.findMaxStudentId();
+    String nextStudentId = generateNextStudentId(maxStudentId);
+    student.setId(nextStudentId);
+
+    repository.insertStudent(student);
+
+    // =====================
+    // course ID 採番
+    // =====================
+    String maxCourseId = repository.findMaxStudentCourseId();
+    String nextCourseId = generateNextStudentCourseId(maxCourseId);
+
+    course.setId(nextCourseId);
+    course.setStudentId(nextStudentId);
+
+    course.setStartDate(LocalDate.now()); // 登録日
+    course.setEndDate(null);              // 終了日なし
+
+    repository.insertStudentCourse(course);
+  }
+
+  public StudentDetail getStudentDetailForEdit(String studentId) {
+    Student student = repository.findStudentById(studentId);
+    List<StudentsCourses> courses = repository.findCoursesByStudentId(studentId);
+
+    StudentDetail detail = new StudentDetail();
+    detail.setStudent(student);
+    detail.setCourses(courses);
+    return detail;
+  }
+
+  private String generateNextStudentId(String maxId) {
+    if (maxId == null) return "stu-001";
+    int num = Integer.parseInt(maxId.substring(4));
+    return String.format("stu-%03d", num + 1);
+  }
+
+  private String generateNextStudentCourseId(String maxId) {
+    if (maxId == null) return "sc-001";
+    int num = Integer.parseInt(maxId.substring(3));
+    return String.format("sc-%03d", num + 1);
+  }
+
+  public void updateStudent(StudentDetail studentDetail, boolean deleteChecked) {
+
+    Student student = studentDetail.getStudent();
+    //StudentsCourses course = studentDetail.getCourses().get(0);
+    if (deleteChecked) {
+      // 論理削除
+      repository.logicalDeleteStudent(student.getId());
+    } else {
+      // 通常更新
+      repository.updateStudent(student);
+    }
+  }
+
+}
